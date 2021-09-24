2b884e3ac51fe405c3787796db91ee9684d284ed
flo_h <flo.1244@gmail.com>
2021-09-24 16:07:24 -0400

    Updated JS and custom CSS

--- js/script.js
+++ js/script.js
@@ -1,13 +1,42 @@
-const button = document.querySelector("button");
-const authorSpan = document.querySelector(".author");
-const imgDiv = document.querySelector(".image-container");
-const img = document.querySelector(".img");
+	const button = document.querySelector("button"); 
+	const authorSpan = document.querySelector(".author");
+	const imgDiv = document.querySelector(".image-container");
+	const img = document.querySelector(".img");
+	
+	
 
-const getImage = async function () {
-	const res = await fetch (
-	"https://picsum.photos/v2/list?limit=100"
-	);
-	const images = await res.json(); 
-	console.log(images);
-};
-getImage();
+	const getImage = async function () {
+		const res = await fetch (
+		"https://picsum.photos/v2/list?limit=100"
+		);
+		const images = await res.json(); 
+		//console.log(images);
+		selectRandomImage(images);
+	};
+
+
+	const selectRandomImage = function (images){
+		const randomIndex = Math.floor(Math.random() * images.length);
+		//console.log(randomIndex);
+		const randomImage = images[randomIndex];
+		//console.log(randomImage);
+		displayImage(randomImage);
+	};
+
+	const displayImage = function (randomImage) {
+		const author = randomImage.author;
+		const imageAddress = randomImage.download_url;
+		authorSpan.innerHTML = author;
+		img.src = imageAddress;
+		img.classList.add("image-fade-in");
+		imgDiv.classList.remove("hide");
+		
+	
+	};
+
+	button.addEventListener("click",function(){
+		img.classList.remove("image-fade-in");
+		getImage();
+	});
+
+
